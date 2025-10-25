4e8de3342664adb6b3d39a31604dad2e1b1ca36a
Kelson Cantrell <cantrellkelson@gmail.com>
2025-10-25 16:15:18 -0700

    basic functionality

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-# app.py
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import numpy as np
 
 app = Flask(__name__)
 
@@ -8,26 +8,61 @@ HTML = """
 <html>
   <body style="margin:0; font-family:sans-serif;">
     <div style="position:absolute; top:10px; left:10px;">
-      <div>hello world</div>
-      <form method="POST" style="margin-top:8px;">
-        <input name="text" type="text" placeholder="type here…"
-               style="width:240px; padding:4px;" value="{value}">
+      <div id="problem" style="margin-top:8px; color:#555;"></div>
+
+      <form id="form" style="margin-top:8px;">
+        <input name="text" id="text" type="text" placeholder="type here…"
+               style="width:240px; padding:4px;">
         <button type="submit" style="padding:4px 8px; margin-left:4px;">Submit</button>
       </form>
-      {output}
+      <div id="output" style="margin-top:8px; color:#555;"></div>
     </div>
+
+    <script>
+      const problem = document.getElementById('problem');
+      const form = document.getElementById('form');
+      const output = document.getElementById('output');
+
+      form.addEventListener('submit', async (e) => {
+        e.preventDefault(); // stop page refresh
+        const text = document.getElementById('text').value;
+
+        // send to Flask asynchronously
+        const response = await fetch('/process', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({text})
+        });
+
+        const data = await response.json();
+        problem.textContent = data.problem;
+        output.textContent = data.output;
+      });
+    </script>
   </body>
 </html>
 """
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        text = (request.form.get("text") or "").strip()
-        out = f'<div style="margin-top:8px; color:#555;">You typed: {text}</div>'
-        return HTML.format(value=text, output=out)
-    # GET
-    return HTML.format(value="", output="")
+@app.route("/")
+def index():
+    return HTML
+
+@app.route("/process", methods=["POST"])
+def process():
+    data = request.get_json()
+    text = (data.get("text") or "").strip()
+    # print(text)
+    try:
+        num = int(text)
+    except:
+        num = "enter an integer!!"
+
+    num1 = np.random.randint(0,10)
+    num2 = np.random.randint(0,10)
+    problem = f"{num1} + {num2}"
+    output = "Correct!" if num == num1+num2 else "Wrong!"
+    return jsonify({"output": f"{output}",
+                    "problem": problem})
 
 if __name__ == "__main__":
     app.run(debug=True)
